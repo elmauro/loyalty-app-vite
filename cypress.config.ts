a4a81749dbe07466fc643fe1639b79f5accaa21a
Mauricio <mauricio@bitstop.co>
2025-04-28 19:15:27 -0500

    Change configuration port

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import vitePreprocessor from 'cypress-vite';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:51730',
     setupNodeEvents(on, config) {
       // Vite support
       on('file:preprocessor', vitePreprocessor());
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 51730,
+  },
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
